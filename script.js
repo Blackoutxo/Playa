621c6aa0b970e7030012729f9de19fe117d9bb08
Blackout180 <Semperx123@gmail.com>
2026-05-28 09:46:20 +0545

    add settings

--- script.js
+++ script.js
@@ -9,6 +9,31 @@ setInterval(() => {
 const settingsBar = document.querySelector('.settings');
 const settingBtn = document.querySelector('.settings-icon');
 
+const toggleBtn = document.querySelector('.toggle-button');
+const toggleableBtn = document.querySelector('.toggleable-button');
+let toggle = 0;
+
 settingsBar.classList.add('hidden');
 
+settingBtn.addEventListener('click', () => {
+    settingsBar.classList.add('hidden');
 
+    // Animation settings icon
+    settingBtn.classList.remove('clickedRev');
+    settingBtn.classList.add('clicked');
+    toggle += 1;
+
+    if(toggle === 2) {
+        settingBtn.classList.remove('clicked');
+        settingBtn.classList.add('clickedRev');
+
+        settingsBar.classList.remove('hidden');
+
+        toggle = 0;
+    }
+});
+
+toggleBtn.addEventListener('click', () => {
+    console.log('clicked toggle button');
+    toggleableBtn.classList.add('clicked');
+});
