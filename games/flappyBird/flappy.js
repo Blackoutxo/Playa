99c933cb0028c31fecd0213712a6e694ab1abbff
Blackout180 <Semperx123@gmail.com>
2026-06-14 13:22:18 +0545

    Just made the pipeline move for now

--- games/flappyBird/flappy.js
+++ games/flappyBird/flappy.js
@@ -6,7 +6,63 @@ const ctx = canvas.getContext('2d');
 let hitbox;
 var bird;
 
+let x, y;
+
 let WIDTH = window.innerWidth;
 let HEIGHT = window.innerHeight;
 
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
+let keyDown = "";
+
 // Game update
+
+    window.addEventListener('keydown', (e) => {
+        if (e.key === "ArrowRight")
+        keyDown = "RIGHT";
+
+        if (e.key === "ArrowLeft")
+            keyDown = "LEFT";
+    });
+
+// Drawing
+x= 100;
+y= 0;
+
+setInterval(() => {
+if (keyDown === "RIGHT") {
+    x += 1;
+
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+} else if (keyDown === "LEFT") {
+    x -= 1;
+
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+    let topX = x;
+    let topY = y;
+
+    let topTX = 90;
+    let topTY = 100;
+
+    ctx.fillStyle = "rgb(19, 228, 54)";
+    ctx.strokeStyle = "rgb(0, 0, 0)";
+    ctx.lineWidth= 6;
+    ctx.strokeRect(topX, topY, topTX, topTY);
+
+    ctx.fillRect(topX, topY, topTX, topTY);
+    ctx.fillRect(topX - 30, topTY, 150, 50);
+
+    ctx.lineWidth = 3;
+    ctx.strokeRect(topX - 30, topTY, 150, 50);
+
+    drawOutline();
+}, 0.01);
+
+function drawOutline() {
+    ctx.strokeStyle = "rgb(61, 92, 228)";
+    ctx.lineWidth = (WIDTH * 0.005);
+    ctx.strokeRect(0, 0, WIDTH, HEIGHT);
+}
