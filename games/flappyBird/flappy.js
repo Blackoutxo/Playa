eec6ccf899d7214c3ff1518644eaa67eb89a6815
Blackout180 <Semperx123@gmail.com>
2026-06-14 16:36:09 +0545

    Some animations and trying to get a grip on how to do the game

--- games/flappyBird/flappy.js
+++ games/flappyBird/flappy.js
@@ -2,67 +2,41 @@
 const canvas = document.getElementById('game-map');
 const ctx = canvas.getContext('2d');
 
+// Docs
+const initScreen = document.querySelector('.first-screen');
+const startButton = document.querySelector('.start-button');
+
 // Field variables
-let hitbox;
-var bird;
-
-let x, y;
-
 let WIDTH = window.innerWidth;
 let HEIGHT = window.innerHeight;
 
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
-let keyDown = "";
+let key = "";
+
+// Check start button
+startButton.addEventListener('click', (e) => {
+    initScreen.classList.add('hide');
+
+    setTimeout(() => {
+        initScreen.style = "transform: translateY(-100%);";
+    }, 1200);
+});
 
 // Game update
-
-    window.addEventListener('keydown', (e) => {
-        if (e.key === "ArrowRight")
-        keyDown = "RIGHT";
-
-        if (e.key === "ArrowLeft")
-            keyDown = "LEFT";
-    });
-
-// Drawing
-x= 100;
-y= 0;
-
 setInterval(() => {
-if (keyDown === "RIGHT") {
-    x += 1;
 
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-} else if (keyDown === "LEFT") {
-    x -= 1;
 
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-}
-
-    let topX = x;
-    let topY = y;
-
-    let topTX = 90;
-    let topTY = 100;
-
-    ctx.fillStyle = "rgb(19, 228, 54)";
-    ctx.strokeStyle = "rgb(0, 0, 0)";
-    ctx.lineWidth= 6;
-    ctx.strokeRect(topX, topY, topTX, topTY);
-
-    ctx.fillRect(topX, topY, topTX, topTY);
-    ctx.fillRect(topX - 30, topTY, 150, 50);
-
-    ctx.lineWidth = 3;
-    ctx.strokeRect(topX - 30, topTY, 150, 50);
-
-    drawOutline();
-}, 0.01);
+}, 1);
 
 function drawOutline() {
     ctx.strokeStyle = "rgb(61, 92, 228)";
     ctx.lineWidth = (WIDTH * 0.005);
     ctx.strokeRect(0, 0, WIDTH, HEIGHT);
 }
+
+// Input handler
+window.addEventListener('keydown', (e) => {
+    
+});
