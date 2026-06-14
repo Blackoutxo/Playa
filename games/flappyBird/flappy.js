1f1a7640af99c5dd8705d562cefd15a917d20675
Blackout180 <Semperx123@gmail.com>
2026-06-14 12:38:29 +0545

    Add Flappy bird

---
+++ games/flappyBird/flappy.js
@@ -0,0 +1,12 @@
+// Canvas init
+const canvas = document.getElementById('game-map');
+const ctx = canvas.getContext('2d');
+
+// Field variables
+let hitbox;
+var bird;
+
+let WIDTH = window.innerWidth;
+let HEIGHT = window.innerHeight;
+
+// Game update
