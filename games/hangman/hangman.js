cd4b7bb1b32550607a0d4f0a859647b90628da8f
Blackout180 <Semperx123@gmail.com>
2026-06-28 23:00:40 +0545

    Add questions to the topics

--- games/hangman/hangman.js
+++ games/hangman/hangman.js
@@ -24,11 +24,49 @@ const topicAutomotive = [
 ];
 
 const topicComputer = [
-
+    {Q: "What popular operating system, launched in 1991, also has its own mascot, Tux the penguin?", A: "LINUX"},
+    {Q: "Which acronym for a computer’s main memory system sounds like a Daft Punk album or a horned mountain animal?", A: "RAM"},
+    {Q: "What term is used for a testing environment in a computer system in which new or untested software or coding can be run securely? In a different context, this term refers to a children’s play area.", A: "SANDBOX"},
+    {Q: "What delicious computer term did web browser programmer Lou Montulli coin to refer to information that is sent from the browser to the web server?", A: "COOKIE"},
+    {Q: "Which two-word term describes computer code that is freely available for distribution and modification, based on a philosophy of improvement through sharing and collaboration?", A: "OPENSOURCE"},
+    {Q: "What is the somewhat-logical term for the internet prank and meme in which an unexpected appearance of the music video for the 1987 Rick Astley song 'Never Gonna Give You Up' is 'planted' into an unrelated link? The meme's popularity dates back to 2008.", A: "RICKROLLING"},
+    {Q: "What is the 6-letter term for a group of computers that is networked together and used by hackers to steal information?", A: "BOTNET"},
+    {Q: "What search engine sounds like someone excited?", A: "YAHOO"},
+    {Q: "Which web browser did microsoft talk about when they tweeted about 'Who else is edging today?' ?", A: "EDGE"},
+    {Q: "Which part of the computer gives birth to it so that it actually works?", A: "MOTHERBOARD"},
+    {Q: "Which 3 letter word for a cable is held down by screws on a regular computer and is considered to be GOATED?", A: "VGA"},
+    {Q: "Which programming language is named after a type of Indonesian coffee?", A: "JAVA"},
+    {Q: "Before it was called Gmail, the 2004 original was an email service for fans of what lasagna-addicted comic strip cat?", A: "GARFIELD"},
+    {Q: "What does “404” mean on a website?", A: "NOTFOUND"},
+    {Q: "Which 2006 home video game console pioneered the use of motion-sensing controllers, getting people to actually stand up and move?", A: "WII"},
+    {Q: "Which feature is often used to verify humans on websites?", A: "CAPTCHA"},
+    {Q: "In quantum computing, what is the term for a unit of quantum information equivalent to a classical bit, but capable of being in superposition", A: "QUBIT"},
+    {Q: "What do you call a picture on a computer screen that you click to open a program or a file?", A: "ICON"},
+    {Q: "What do you call a secret word or phrase that you use to prove it’s you when logging into a website?", A: "PASSWORD"},
+    {Q: "What do we call a small, portable computer that you can carry on your lap?", A: "LAPTOP"},
+    {Q: "What is the term for malicious software designed to damage computers?", A: "MALWARE"},
+    {Q: "What programming language is primarily used for web development alongside HTML and CSS?", A: "JAVASCRIPT"},
+    {Q: "What do you call a handheld device or perhaps a little tiny computer that fits into our pockets?", A: "MOBILE"},
 ];
 
 const topicGeneralGame = [
-
+    {Q: "What 2009 game, developed by Mojang, is an open sandbox in which players often build structures and battle creepers, zombies, and other 'mobs'?", A: "MINECRAFT"},
+    {Q: "Originally given the Japanese title “Puckman,” what 1980s arcade game was inducted into the Guinness Book of Records as the 'Most Successful Coin-Operated Game' in 2005?", A: "PACMAN"},
+    {Q: "What Will Wright created video game series, released in 2000 (with sequels in 2004, 2009, and 2014), saw players watching and directing characters to mundane things like eating, sleeping, and cleaning their houses?", A: "THESIMS"},
+    {Q: "Making his debut in 1990s 'Super Mario World,'' what is the name of the enemy-eating, egg-throwing green dinosaur who serves as a sidekick to Mario and Luigi in the Mario franchise?", A: "YOSHI"},
+    {Q: "In what video game universe, created by Capcom, would you find a character named Jill Valentine? The name of this game could also mean a home-dwelling spirit.", A: "RESIDENTEVIL"},
+    {Q: "What is the name of the plumber who is Nintendo’s most famous character?", A: "MARIO"},
+    {Q: " What is the default skin character’s name in Minecraft?", A: "STEVE"},
+    {Q: "In Among Us, what do players call the hidden villain?", A: "IMPOSTER"},
+    {Q: "Which video game series features Master Chief as the main character?", A: "HALO"},
+    {Q: "What is the highest-selling video game of all time?", A: "MINECRAFT"},
+    {Q: "In Red Dead Redemption 2, what is the name of the main character who is a member of the Van der Linde gang?", A: "ARTHURMORGAN"},
+    {Q: "Speaking of Red Dead Remption 2, What did arthur die of?", A: "TUBERCULOSIS"},
+    {Q: "In Hollow Knight, what is the name of the main character, a silent knight who explores the kingdom of Hallownest?", A: "THEKNIGHT"},
+    {Q: "What is the name of the cheat code “Up, Up, Down, Down, Left, Right, Left, Right, B, A” associated with many Konami games?", A: "KONAMICODE"},
+    {Q: "Which 2013 mobile game was removed from the App Store by its creator due to concerns over its addictive nature?", A: "FLAPPYBIRD"},
+    {Q: "Which classic puzzle game involves arranging falling blocks to complete lines?", A: "TETRIS"},
+    {Q: ""}
 ];
 
 const topicAircraft = [
@@ -122,7 +160,7 @@ function setUpQuestion(topic) {
 
 // Input log
 window.addEventListener('keydown', (e) => {
-    if (strike < 0) return;
+    if (strike <= 0) return;
 
     // Fetch input key
     typed = e.key.toUpperCase();
