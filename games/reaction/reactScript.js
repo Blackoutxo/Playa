1a7f3cda8079cbeb617972d35a83766e42b8b1d9
Blackout180 <Semperx123@gmail.com>
2026-09-06 18:00:10 +0545

    Add animation counter, hide elements that need to be hidden initially, add current time, current best time in vars

--- games/reaction/reactScript.js
+++ games/reaction/reactScript.js
@@ -1,4 +1,12 @@
+// Theme var
 const theme = localStorage.getItem('theme');
+
+// Stat vars
+const bestTime = document.querySelector('.BMS');
+const currentTime = document.querySelector(".CMS");
+
+// General vars
+const count = document.querySelector('.count');
 
 // Change theme
 if (theme === '1') {
@@ -9,3 +17,23 @@ if (theme === '1') {
 setTimeout(() => {
     document.querySelector('.loading-screen').classList.add('zoom');
 }, 3000);
+
+// hide elements that need hiding
+count.classList.add('hide');
+
+// Add animation counter
+function animateCounter(start, target, elm) {
+    const duration = 2000;
+    elm.textContent = start;
+
+    function update(now) {
+        const t = Math.min((now - start) / duration, 1);
+        const ease = 1 - Math.pow(1 - t, 3);
+        elm.textContent = Math.round(ease * target);
+
+        if (t < 1) requestAnimationFrame(update);
+        else elm.textContent = target;
+    }
+
+    requestAnimationFrame(update);
+}
